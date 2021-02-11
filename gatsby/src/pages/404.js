968b7b6637911c441a4d35c26e5bba04be3ced5b
danleks <danilevich.lks@gmail.com>
2021-02-11 22:35:59 +0100

    Create 404.js

---
+++ gatsby/src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FourOhFourPage = () => (
+        <div>
+                <p>hey, that page doesn't exist</p>
+        </div>
+);
+
+export default FourOhFourPage;
