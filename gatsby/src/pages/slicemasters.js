7cca2aca1802cdf500cb69bd236c1c34b37c9fdd
danleks <danilevich.lks@gmail.com>
2021-02-11 22:35:15 +0100

    Create slicemasters.js

---
+++ gatsby/src/pages/slicemasters.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SlicemastersPage = () => (
+        <div>
+                <p>Hey, i am a SlicemastersPage</p>
+        </div>
+);
+
+export default SlicemastersPage;
