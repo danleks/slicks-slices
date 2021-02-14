c688b06a81f354015cea861098a0f1ab82f22792
danleks <danilevich.lks@gmail.com>
2021-02-14 13:53:29 +0100

    Create topping.js

---
+++ sanity/schemas/topping.js
@@ -0,0 +1,34 @@
+import { FaPepperHot as icon } from 'react-icons/fa';
+
+export default {
+  name: 'topping',
+  title: 'Toppings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Name of the topping',
+      type: 'string',
+      description: 'Name of the topping',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+      description: 'Is it vegetarian?',
+      options: {
+        layout: 'checkbox',
+      },
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian',
+    },
+    prepare: ({ name, vegetarian }) => ({
+      title: `${name} ${vegetarian ? '🥬' : ''}`,
+    }),
+  },
+};
