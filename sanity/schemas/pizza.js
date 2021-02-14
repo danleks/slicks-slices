42d5a02b29f13478dac88f00fe26982496d1327e
danleks <danilevich.lks@gmail.com>
2021-02-14 13:53:09 +0100

    Create pizza.js

---
+++ sanity/schemas/pizza.js
@@ -0,0 +1,69 @@
+import { MdLocalPizza as icon } from 'react-icons/md';
+import PriceInput from '../components/Priceinput';
+
+export default {
+  name: 'pizza',
+  title: 'Pizzas',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza Name',
+      type: 'string',
+      description: 'Name of the Pizza',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+      description: 'Price of the pizza in zl',
+      inputComponent: PriceInput,
+      validation: (Rule) => Rule.min(1000).max(50000),
+      // Todo: add custom component
+    },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ name, media, ...toppings }) => {
+      const tops = Object.values(toppings).filter((topping) =>
+        Boolean(topping)
+      );
+      return {
+        title: name,
+        media,
+        subtitle: tops.join(', '),
+      };
+    },
+  },
+};
