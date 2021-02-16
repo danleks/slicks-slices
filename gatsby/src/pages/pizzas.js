76105858b77cbe17a0d37813b754b664228783bb
danleks <danilevich.lks@gmail.com>
2021-02-16 22:13:47 +0100

    Update pizzas.js

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,9 +1,44 @@
+import { graphql } from 'gatsby';
 import React from 'react';
+import PizzaList from '../components/PizzaList/PizzaList';
+import ToppingsFilter from '../components/ToppingsFilter/ToppingsFilter';
 
-const PizzasPage = () => (
-        <div>
-                <p>Hey, i am a PizzasPage</p>
-        </div>
-);
+const PizzasPage = ({ data }) => {
+   const pizzas = data.pizzas.nodes;
+   return (
+      <div>
+         <ToppingsFilter />
+         <PizzaList pizzas={pizzas} />
+      </div>
+   );
+};
+
+export const query = graphql`
+   query PizzaQuery {
+      pizzas: allSanityPizza {
+         nodes {
+            name
+            id
+            slug {
+               current
+            }
+            toppings {
+               id
+               name
+            }
+            image {
+               asset {
+                  fixed(width: 200, height: 200) {
+                     ...GatsbySanityImageFixed
+                  }
+                  fluid(maxWidth: 400) {
+                     ...GatsbySanityImageFluid
+                  }
+               }
+            }
+         }
+      }
+   }
+`;
 
 export default PizzasPage;
