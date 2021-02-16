f50c0e410eafdbc71bcbe74b153debc81a653135
danleks <danilevich.lks@gmail.com>
2021-02-16 22:13:35 +0100

    Create ToppingsFilter styles

---
+++ gatsby/src/components/ToppingsFilter/ToppingsFilter.styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const ToppingsStyles = styled.div`
+   display: flex;
+   flex-wrap: wrap;
+   gap: 1rem;
+   margin-bottom: 4rem;
+
+   a {
+      display: grid;
+      grid-template-columns: auto 1fr;
+      align-items: center;
+      gap: 0 1rem;
+      padding: 5px;
+      border-radius: 2px;
+      background-color: var(--grey);
+
+      .count {
+         background-color: var(--white);
+         padding: 2px 5px;
+      }
+
+      .active {
+         background-color: var(--yellow);
+      }
+   }
+`;
