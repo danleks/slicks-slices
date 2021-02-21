5d4753219f30ff6ac783a02781bcf90d01ac434e
danleks <danilevich.lks@gmail.com>
2021-02-21 21:54:45 +0100

    Create Pizza styles

---
+++ gatsby/src/templates/Pizza/Pizza.styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const PizzaGridStyles = styled.div`
+   display: grid;
+   gap: 2rem;
+   grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+`;
