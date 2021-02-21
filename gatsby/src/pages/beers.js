a0a6cfc575a2f6ec0be0c9cc3c53514de65b2c1c
danleks <danilevich.lks@gmail.com>
2021-02-21 21:54:19 +0100

    Update beers.js

--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -1,9 +1,42 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 
-const BeersPage = () => (
-        <div>
-                <p>Hey, i am a BeersPage</p>
-        </div>
-);
+const BeersPage = ({ data }) => {
+   console.log('i');
+   return (
+      <>
+         <h2 className="center">We have {data.beers.nodes.length} beers available. Dine in Only!</h2>
+         <div>
+            {data.beers.nodes.map((beer) => {
+               console.log(beer.name);
+               return (
+                  <div key={beer.id}>
+                     <img src={beer.image} alt={beer.name} />
+                     <h3>{beer.name}</h3>
+                     {beer.price}
+                  </div>
+               );
+            })}
+         </div>
+      </>
+   );
+};
 
 export default BeersPage;
+
+export const query = graphql`
+   query {
+      beers: allBeer {
+         nodes {
+            id
+            name
+            image
+            price
+            rating {
+               average
+               reviews
+            }
+         }
+      }
+   }
+`;
