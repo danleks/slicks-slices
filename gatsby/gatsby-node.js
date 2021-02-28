65f22d4d2798d6602adf1a7ccc176e5a227f3e21
danleks <danilevich.lks@gmail.com>
2021-02-28 18:00:27 +0100

    Update gatsby-node.js

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -84,6 +84,40 @@ async function fetchBeersAndTurnIntoNodes({ actions, createNodeId, createContent
    }
 }
 
+async function turnSlicemastersIntoPages({ graphql, actions }) {
+   // 1. Query all slicemasters
+   const { data } = await graphql(`
+      query {
+         slicemasters: allSanityPerson {
+            totalCount
+            nodes {
+               id
+               name
+               slug {
+                  current
+               }
+            }
+         }
+      }
+   `);
+   // TODO: 2. Turn each slicemaster into their page
+   // 3. Figure out how many pages there are based on how many slicemasters there are, and how many per page!
+   const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE);
+   const pageCount = Math.ceil(data.slicemasters.totalCount / pageSize);
+   // 4. Loop from 1 to n and crate pages for them
+   Array.from({ length: pageCount }).map((_, i) => {
+      actions.createPage({
+         path: `slicemasters/${i + 1}`,
+         component: path.resolve('./src/pages/slicemasters.js'),
+         context: {
+            skip: i * pageSize,
+            currentPage: i + 1,
+            pageSize,
+         },
+      });
+   });
+}
+
 export async function sourceNodes(params) {
    // fetch a list of beers and source them into our gatsby API
    await Promise.all([fetchBeersAndTurnIntoNodes(params)]);
@@ -93,6 +127,6 @@ export async function createPages(params) {
    // create pages dynamically
    // 1. pizzas
    // 2. toppings
-   await Promise.all([turnPizzasIntoPages(params), turnToppingsIntoPages(params)]);
+   await Promise.all([turnPizzasIntoPages(params), turnToppingsIntoPages(params), turnSlicemastersIntoPages(params)]);
    // 3. slicemasters
 }
