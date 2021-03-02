f961040ad6d5643e33a1b6aa43555d73c106245f
danleks <danilevich.lks@gmail.com>
2021-03-02 22:43:53 +0100

    Update Pizza.js

--- gatsby/src/templates/Pizza/Pizza.js
+++ gatsby/src/templates/Pizza/Pizza.js
@@ -2,21 +2,25 @@ import { graphql } from 'gatsby';
 import React from 'react';
 import Img from 'gatsby-image';
 import { PizzaGridStyles } from './Pizza.styles';
+import SEO from '../../components/SEO/SEO';
 
 const SinglePizzaPage = ({ data }) => {
    const { pizza } = data;
    return (
-      <PizzaGridStyles>
-         <Img fluid={pizza.image.asset.fluid} />
-         <div>
-            <h2 className="mark">{pizza.name}</h2>
-            <ul>
-               {pizza.toppings.map((topping) => (
-                  <li key={topping.id}>{topping.name}</li>
-               ))}
-            </ul>
-         </div>
-      </PizzaGridStyles>
+      <>
+         <SEO title={pizza.name} image={pizza.image?.asset?.fluid?.src} />
+         <PizzaGridStyles>
+            <Img fluid={pizza.image.asset.fluid} />
+            <div>
+               <h2 className="mark">{pizza.name}</h2>
+               <ul>
+                  {pizza.toppings.map((topping) => (
+                     <li key={topping.id}>{topping.name}</li>
+                  ))}
+               </ul>
+            </div>
+         </PizzaGridStyles>
+      </>
    );
 };
 
