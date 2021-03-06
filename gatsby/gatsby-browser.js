f394d21ba55314291f30e35ca71d303f26af6696
danleks <danilevich.lks@gmail.com>
2021-03-06 20:25:49 +0100

    Update gatsby-browser.js

--- gatsby/gatsby-browser.js
+++ gatsby/gatsby-browser.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import Layout from './src/components/Layout/Layout';
+import { OrderProvider } from './src/components/OrderContext/OrderContext';
 
 export const wrapPageElement = ({ element, props }) => <Layout {...props}>{element}</Layout>;
+
+export const wrapRootElement = ({ element }) => <OrderProvider>{element}</OrderProvider>;
